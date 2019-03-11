f0aeee4c4d95ab18e225ac110ea8d57105353beb
juanpad94 <juanpad94@gmail.com>
2019-03-10 18:14:49 -0700

    photo url

---
+++ app/src/main/java/com/example/flickrfindr/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.flickrfindr;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    // PHOTOS WILL BE SEARCHED BY TEXT (WHICH CONTAIN TAGS;
+    // ALTHOUGH THEY CAN BE SEARCHED EXCLUSIVELY BY TAGS, AMONG OTHER PARAMETERS
+    // https://www.flickr.com/services/api/flickr.photos.search.html
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        String apiKey = "1508443e49213ff84d566777dc211f2a";
+        String searchWord = "cristiano ronaldo";
+        String url = "https://api.flickr.com/services/rest/?method=flickr.photos.search" +
+                "&api_key=" + apiKey +
+                "&text=" + searchWord +
+                "&per_page=25" +
+                "&format=json&nojsoncallback=1";
+    }
+}
---
+++ app/src/main/java/com/example/flickrfindr/ResultsActivity.java
@@ -0,0 +1,16 @@
+package com.example.flickrfindr;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
+public class ResultsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_results);
+
+    }
+}
