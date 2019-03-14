24911eb70351fecddeb7bef966b1ed5bc87ea797
Juan Padilla <padilla.juan@solute.us>
2019-03-14 11:18:27 -0700

    add main menu and results button

--- app/src/main/java/com/example/flickrfindr/PhotoActivity.kt
+++ app/src/main/java/com/example/flickrfindr/PhotoActivity.kt
@@ -1,10 +1,13 @@
 package com.example.flickrfindr
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.graphics.Bitmap
 import android.widget.FrameLayout
 import android.os.Bundle
 import android.os.Parcelable
+import android.view.View
+import android.widget.Button
 import android.widget.ImageView
 
 class PhotoActivity : AppCompatActivity() {
@@ -18,5 +21,19 @@ class PhotoActivity : AppCompatActivity() {
         flickrPhoto.setImageBitmap(bitMap)
         val frameLay = findViewById(R.id.frameLayout) as FrameLayout
         frameLay.addView(flickrPhoto)
+        val searchButton = findViewById<View>(R.id.search_button) as Button
+        searchButton.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(v: View) {
+                val searchIntent = Intent(this@PhotoActivity, MainActivity::class.java)
+                startActivity(searchIntent)
+            }
+        })
+        val resultsButton = findViewById(R.id.results_button) as Button
+        resultsButton.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(v: View) {
+                val searchIntent = Intent(this@PhotoActivity, ResultsActivity::class.java)
+                startActivity(searchIntent)
+            }
+        })
     }
 }
